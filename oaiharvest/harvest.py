bloomonkey/oai-harvest

commit 6d85d4035b6cd1362af288bda33579050285424a

added capability to selectively harvest by set
Closes #2

oaiharvest/harvest.py
@@ -182,7 +182,8 @@ def main(argv=None):
             harvester.harvest(baseUrl,
                               args.metadataPrefix,
                               from_=args.from_,
-                              until=args.until
+                              until=args.until,
+                              set=args.set
                               )
         except NoRecordsMatchError:
             # Nothing to harvest
@@ -242,6 +243,10 @@ argparser.add_argument("-u", "--until", dest="until",
                        metavar="YYYY-MM-DD",
                        help=("harvest only records added/modified up to this "
                              "date.")
+                       )
+argparser.add_argument("-s", "--set", dest="set",
+                       default=None,
+                       help=("harvest only records within this set")
                        )
 
 group = argparser.add_mutually_exclusive_group()

commit 15c1a5bbfb5e54b5e754acdc8f853e4943a8437f

bugfix: harvesting from servers without sets
Fixes bug introduced by 6d85d4035b6cd1362af288bda33579050285424a
whereby servers that don't support sets would always raise
`NoSetHierarchyError`

oaiharvest/harvest.py
@@ -179,12 +179,20 @@ def main(argv=None):
         # Any records added after this snapshot, but before completion of
         # harvesting should be included in next harvest.
         harvestTime = datetime.now()
+        # Create a dictionary of keyword args
+        # Avoid sending kwargs with value of None - e.g. set=None causes
+        # error on servers that don't support set hierarchy.
+        kwargs = {}
+        if args.from_ is not None:
+            kwargs['from_'] = args.from_
+        if args.until is not None:
+            kwargs['until'] = args.until
+        if args.set is not None:
+            kwargs['set'] = args.set
         try:
             harvester.harvest(baseUrl,
                               args.metadataPrefix,
-                              from_=args.from_,
-                              until=args.until,
-                              set=args.set
+                              **kwargs
                               )
         except NoRecordsMatchError:
             # Nothing to harvest