9aa587773a7e172a890d6b5288568c09dc63a8ae
John Harrison <john.harrison@liv.ac.uk>
2013-05-09 17:08:32 +0100

    updated setup for development of 1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ py_version = getattr(sys, 'version_info', (0, 0, 0))
 
 # Basic information
 _name = "oaiharvest"
-_version = "1.1"
+_version = "1.2"
 _description = ("A harvester to collect records from an OAI-PMH enabled "
                 "provider.")
 _author = 'John Harrison'
