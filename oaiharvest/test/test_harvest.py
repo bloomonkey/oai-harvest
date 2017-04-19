24d0133f397ddea51c71f1ea4bbd88100c54a403
John Harrison <john.harrison@autotrader.co.uk>
2017-04-19 20:03:13 +0100

    Add test for _listRecords

--- oaiharvest/test/test_harvest.py
+++ oaiharvest/test/test_harvest.py
@@ -13,6 +13,7 @@ from tempfile import TemporaryDirectory
 from mock import Mock, patch
 from oaipmh.metadata import MetadataRegistry
 
+from oaiharvest.exceptions import NotOAIPMHBaseURLException
 from oaiharvest.harvest import OAIHarvester, DirectoryOAIHarvester
 
 
@@ -50,6 +51,36 @@ class DirectoryOAIHarvesterrTestCase(unittest.TestCase):
         self.assertTrue(self.harvester.respectDeletions)
         self.assertFalse(self.harvester.createSubDirs)
         self.assertEqual(self.harvester.nRecs, 0)
+
+    @patch('oaiharvest.harvest.Client')
+    def test_listRecords_on_non_OAI_target(self, MockClient):
+        client = MockClient.return_value
+        client.identify.side_effect = IndexError
+        url = 'https://www.example.com'
+
+        with self.assertRaises(NotOAIPMHBaseURLException):
+            for rec in self.harvester._listRecords(url):
+                pass
+
+    @patch('oaiharvest.harvest.Client')
+    def test_listRecords(self, MockClient):
+        client = MockClient.return_value
+        mock_recs = [Mock()]
+        client.listRecords.return_value = iter(mock_recs)
+        url = 'https://oai.example.com'
+
+        recs = self.harvester._listRecords(
+            url,
+            metadataPrefix='oai_dc',
+            foo='bar'
+        )
+        for rec, expected in zip(recs, mock_recs):
+            self.assertEqual(rec, expected)
+
+        client.listRecords.assert_called_once_with(
+            metadataPrefix='oai_dc',
+            foo='bar'
+        )
 
 
 if __name__ == '__main__':
